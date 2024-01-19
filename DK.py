031842c6cba7a5bb6ef2ce6e0a5d0f52440416b5
Jackson Davis <jdaviskid27@yahoo.com>
2024-01-19 18:10:02 -0500

    Can now run it and turn it to a csv

--- DK.py
+++ DK.py
@@ -1,17 +1,24 @@
 import requests
 import pandas as pd
 
-games = []
-
-
-def parse_contest_info(contest_info, prop_name, ):
+def parse_contest_info(contest_info, prop_name):
+    d = []
     for game in contest_info:
         for player in game:
             curr_label = " ".join(player['label'].split()[:2])
             curr_line = player['outcomes'][0]['line']        
             curr_over = player['outcomes'][0]['oddsAmerican']
             curr_under = player['outcomes'][1]['oddsAmerican']
-            print("Player: " + curr_label + ", their " + prop_name +"'s line is set to: " + str(curr_line) + " with the Over odds as: " + curr_over +" and the Under odds as: " + curr_under)
+            
+            playerInfo = {"Player ": curr_label, 
+                 "Prop Name": prop_name,
+                 "Line": curr_line, 
+                 "Over":curr_over, 
+                 "Under": curr_under}
+            
+            d.append(playerInfo)
+            
+    return d
 
 def getNBAPlayerPoints():
 
@@ -20,38 +27,33 @@ def getNBAPlayerPoints():
 
     contest_info = response['eventGroup']['offerCategories'][2]['offerSubcategoryDescriptors'][0]['offerSubcategory']['offers']
 
-    parse_contest_info(contest_info, "points")
+    return parse_contest_info(contest_info, "Points")
 
-    # for game in contest_info:
-    #     for player in game:
-    #         curr_label = " ".join(player['label'].split()[:2])
-    #         curr_line = player['outcomes'][0]['line']        
-    #         curr_over = player['outcomes'][0]['oddsAmerican']
-    #         curr_under = player['outcomes'][1]['oddsAmerican']
-    #         print(f"For the player: " + curr_label + ", their point's line is set to: " + str(curr_line) + " with the Over odds as: " + curr_over +" and the Under odds as: " + curr_under)
 
 def getNBAPlayerRebounds():
 
     website_url = 'https://sportsbook-nash-usma.draftkings.com/sites/US-MA-SB/api/v5/eventgroups/42648/categories/1216?format=json'
-
     response = requests.get(url=website_url).json()
 
     contest_info = response['eventGroup']['offerCategories'][5]['offerSubcategoryDescriptors'][0]['offerSubcategory']['offers']
     
-    parse_contest_info(contest_info, "rebounds")
+    return parse_contest_info(contest_info, "Rebounds")
 
 def getNBAPlayerAssists():
 
     website_url = 'https://sportsbook-nash-usma.draftkings.com/sites/US-MA-SB/api/v5/eventgroups/42648/categories/1217?format=json'
-
     response = requests.get(url=website_url).json()
 
     contest_info = response['eventGroup']['offerCategories'][6]['offerSubcategoryDescriptors'][0]['offerSubcategory']['offers']
     
-    parse_contest_info(contest_info, "assists")
-    assistDF = pd.DataFrame(contest_info)
+    return parse_contest_info(contest_info, "Assists")
 
-getNBAPlayerPoints()
-getNBAPlayerRebounds()
-getNBAPlayerAssists()
+
+
+pointsDataframe = pd.DataFrame(getNBAPlayerPoints())
+reboundsDataframe = pd.DataFrame(getNBAPlayerRebounds())
+assistsDataframe = pd.DataFrame(getNBAPlayerAssists())
+finalDataframe = pd.concat([pointsDataframe, reboundsDataframe, assistsDataframe])
+finalDataframe.to_csv('FinalDKDataframe.csv', index=False)
+
         
