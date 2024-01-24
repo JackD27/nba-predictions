3ccfb1dd6b9a26cebdfb395548d9e19cf77a70b3
Jackson Davis <jdaviskid27@yahoo.com>
2024-01-23 22:22:04 -0500

    spacing

--- DK.py
+++ DK.py
@@ -4,7 +4,7 @@ import pandas as pd
 class DraftKingsWebsite:
     def __init__(self):
         pass
-    
+
     def parse_contest_info(self, contest_info, prop_name):
         d = []
         for game in contest_info:
@@ -13,47 +13,34 @@ class DraftKingsWebsite:
                 curr_line = player['outcomes'][0]['line']        
                 curr_over = player['outcomes'][0]['oddsAmerican']
                 curr_under = player['outcomes'][1]['oddsAmerican']
-                
                 playerInfo = {"Player ": curr_label, 
                     "Prop Name": prop_name,
                     "Line": curr_line, 
                     "Over":curr_over, 
                     "Under": curr_under}
-                
                 d.append(playerInfo)
-                
         return d
 
     def get_NBA_player_points(self):
-
         website_url = 'https://sportsbook-nash-usma.draftkings.com/sites/US-MA-SB/api/v5/eventgroups/42648/categories/1215/subcategories/12488?format=json'
         response = requests.get(url=website_url).json()
-
         contest_info = response['eventGroup']['offerCategories'][3]['offerSubcategoryDescriptors'][0]['offerSubcategory']['offers']
-
         return self.parse_contest_info(contest_info, "Points")
 
 
     def get_NBA_player_rebounds(self):
-
         website_url = 'https://sportsbook-nash-usma.draftkings.com/sites/US-MA-SB/api/v5/eventgroups/42648/categories/1216?format=json'
         response = requests.get(url=website_url).json()
-
         contest_info = response['eventGroup']['offerCategories'][6]['offerSubcategoryDescriptors'][0]['offerSubcategory']['offers']
-        
         return self.parse_contest_info(contest_info, "Rebounds")
 
     def get_NBA_player_assists(self):
-
         website_url = 'https://sportsbook-nash-usma.draftkings.com/sites/US-MA-SB/api/v5/eventgroups/42648/categories/1217?format=json'
         response = requests.get(url=website_url).json()
-
         contest_info = response['eventGroup']['offerCategories'][7]['offerSubcategoryDescriptors'][0]['offerSubcategory']['offers']
-        
         return self.parse_contest_info(contest_info, "Assists")
-
-
-
+    
+    
 if __name__ == "__main__":
     dk = DraftKingsWebsite()
     pointsDataframe = pd.DataFrame(dk.get_NBA_player_points())
